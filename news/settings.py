02dccf814f820fe5c8fa1d5a52ef077e82c732be
Danyil <dselivanov2000@gmail.com>
2020-11-08 17:57:54 +0200

    changed name of static folder for heroku

--- news/settings.py
+++ news/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
