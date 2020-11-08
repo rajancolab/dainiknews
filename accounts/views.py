9b9dddac6792db2eb740bb2ca2a658105214b26b
Danyil <dselivanov2000@gmail.com>
2020-11-08 22:06:45 +0200

    edit pagination

--- accounts/views.py
+++ accounts/views.py
@@ -95,7 +95,7 @@ def profile_all_articles(request):
         tmp = (article, article.comment_set.count(), article.likes+article.dislikes)
         list_of_data.append(tmp)
 
-    paginator = Paginator(list_of_data, 5)
+    paginator = Paginator(list_of_data, 7)
 
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
